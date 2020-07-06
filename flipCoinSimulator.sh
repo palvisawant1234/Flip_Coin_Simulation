229bf8468e9c57589f1e4d2590dfd977abbde174
sawantpalvi1234 <sawantpalvi087@gmail>
2020-07-07 01:34:41 +0530

    Loop multiple times flipping a coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,9 +1,24 @@
-#!/bin/bash -x
+#!/bin/bash 
 
-Random=$((RANDOM % 2))
-if [ $Random -eq 1 ]
+echo Enter the number of times to flip a coin
+read n
+
+for((i=0 ; i<$n ; i++))
+do
+	Random=$((RANDOM % 2))
+	if [ $Random -eq 1 ]
+	then
+        	echo "Heads won"
+		Heads=$(($Heads+1))
+	else
+        	echo "Tails won"
+		Tails=$(($Tails+1))
+	fi
+done
+
+if [ $Heads -lt $Tails ]
 then
-        echo "Heads won"
+	echo "Tails won by $Tails score"
 else
-        echo "Tails won"
+	echo "Heads won by $Heads score"
 fi
